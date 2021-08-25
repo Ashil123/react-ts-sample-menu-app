89f78135cf668b714bfa8c89f09a0cb29395e232
Ashil123 <ashiljayaraj@gmail.com>
2021-08-25 10:59:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-tplz5e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-tplz5e)
---
+++ sampleMenu.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+export const MyMenu = (props) => {
+
+  const [menuVisible, setMenuVisible] = React.useState(false);
+
+  return (
+    <div>
+      <button
+       onClick={() => { setMenuVisible(!menuVisible)}}>
+         Menu List
+      </button>
+        {menuVisible && (
+          <ul style={{ listStyleType: "none" }}>
+            <li>Electronics</li>
+            <li>Vehicles</li>
+            <li>Home Appliaces</li>
+            <li>Grocessories</li>
+          </ul>
+        )}
+    </div>
+  );
+};
